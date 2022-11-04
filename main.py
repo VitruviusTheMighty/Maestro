71a98de992a96c00ecfd5759503105479e91cc96
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2022-11-04 09:22:03 -0400

    Created game select menu

--- main.py
+++ main.py
@@ -36,13 +36,12 @@ class Porfolio:
 
     def load_background(self, use_center=True):
         
-        bg_x = self.background.get_width()
-        bg_y = self.background.get_height()
-
-        cbg_x = bg_x // 2
-        cbg_y = bg_y // 2
-
         if self.background != None:
+            bg_x = self.background.get_width()
+            bg_y = self.background.get_height()
+
+            cbg_x = bg_x // 2
+            cbg_y = bg_y // 2
             coords = (0,0) 
             if use_center: coords = ((self.center_win_width-cbg_x),(self.center_win_height-cbg_y))   
             self.bg_coords = coords
@@ -52,6 +51,15 @@ class Porfolio:
             self.SCREEN.blit(self.background, self.bg_coords)
         else:
             self.SCREEN.fill("black")
+
+    def gen_text(self, text="", size=45, color="#b68f40", pos=(0,0)):
+        TEXT = get_font(size=size).render(text, True, color)
+        TEXT_RECT = TEXT.get_rect(center=pos)
+        return TEXT, TEXT_RECT
+
+    def display_text(self, text="", size=45, color="#b68f40", pos=(0,0)):
+        text, text_rect = self.gen_text(text=text, size=size, pos=pos, color=color)
+        self.SCREEN.blit(text, text_rect)
 
     def prep_portfolio(self, background_filepath=None):
         """
@@ -73,9 +81,11 @@ class Porfolio:
 
             MOUSE_POS = pygame.mouse.get_pos()
 
-            GAME_SELECT = Button(image=pygame.image.load(os.path.join(DIRNAME, "assets//Play Rect.png")), pos=(self.cx, 250), 
+            GAME_SELECT = Button(image=pygame.image.load(os.path.join(DIRNAME, "assets//Play Rect.png")), pos=(self.center_win_width, 250), 
                                 text_input="GAME SELECT", font=get_font(30), base_color="#d7fcd4", hovering_color="#b68f40")
 
+
+            self.display_text(text="Portfolio", size=50, pos=(self.center_win_width,100))
 
             for button in [GAME_SELECT]:
                 button.changeColor(MOUSE_POS)
@@ -89,12 +99,12 @@ class Porfolio:
                     if GAME_SELECT.checkForInput(MOUSE_POS):
                         # self.playgame()
                         # TODO: Do a thing
-
+                        pass
                     # if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     #     self.options()
                     # if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     #     pygame.quit()
-                        sys.exit()
+                        # sys.exit()
             pygame.display.update()
 
 
@@ -105,4 +115,5 @@ class Porfolio:
 
 if __name__ == "__main__":
 
-    pass
+    p = Porfolio(600, 800)
+    p.portfolio_loop()
